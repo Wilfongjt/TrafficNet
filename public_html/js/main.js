6db44595c62f06fd0bd108a569937d516dc42dee
James <wilfongjt@gmail.com>
2017-07-24 14:07:57 -0400

    Add types for car and fire engine, add color blue for car and red for fire engine add fire engine to map key

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -14,8 +14,8 @@ d3.json(url, function (error, graph) {
     //console.log("gisModel: " + JSON.stringify(gisModel));
     loadCars();
     /*for(var i =0; i < gisModel.network.length; i++){
-        console.log("road: " + JSON.stringify(gisModel.getRoad(i)));
-    }*/
+     console.log("road: " + JSON.stringify(gisModel.getRoad(i)));
+     }*/
     var roads = d3.select("body") // initialize the road
             .select("#network")
             .append("g")
@@ -68,52 +68,87 @@ d3.json(url, function (error, graph) {
             .data(cars) // set data array
             .enter() // 
             .append("circle")
-            .attr("cx", function (d) {
+            .attr("cx", function (d) {// d=car
                 //console.log("d: " + JSON.stringify(d));
                 return d.coordinates[0].x;
             })
             .attr("cy", function (d) {
                 return d.coordinates[0].y;
             })
-            .attr("r", 3)
-            .attr("fill", "blue")
+            .attr("r", function (d) {
+                
+                switch (d.type) {
+                    case 1:// normal car
+                        return 4;
+                    case 2: // fire truck
+                        return 5;
+                }
+                return 6;
+            })
+            .attr("fill", function (d) {
+                switch (d.type) {
+                    case 1:// normal car
+                        return "blue";
+                    case 2: // fire truck
+                        return "red";
+                }
+                return "darkgrey";
+            })
             ;
 
     // Legend
+    var sx = 20;
+    var sx2 = 44;
+    var sy = 20
+    var to = 5;
     d3.select("#key")
             .append("circle")
-            .attr("cx", 15)
-            .attr("cy", 20)
+            .attr("cx", sx)
+            .attr("cy", sy)
             .attr("r", 5)
             .attr("fill", "blue")
     d3.select("#key")
             .append("text")
             .style("fill", "black")
-            .attr("x", 44)
-            .attr("y", 24)
+            .attr("x", sx2)
+            .attr("y", sy + to)
             .text("Car");
-
+    /////////
+    d3.select("#key")
+            .append("circle")
+            .attr("cx", sx)
+            .attr("cy", sy * 2)
+            .attr("r", 5)
+            .attr("fill", "red")
+    d3.select("#key")
+            .append("text")
+            .style("fill", "black")
+            .attr("x", sx2)
+            .attr("y", (sy * 2) + to)
+            .text("Fire Engine");
+    //////////
     d3.select("#key")
             .append("line")
-            .attr("x1", 10)
-            .attr("y1", 40)
+            .attr("x1", 0)
+            .attr("y1", (sy * 3))
             .attr("x2", 40)
-            .attr("y2", 40)
+            .attr("y2", (sy * 3))
             .attr("stroke-width", 2)
             .attr("stroke", "darkgray");
     d3.select("#key")
             .append("text")
             .style("fill", "black")
-            .attr("x", 45)
-            .attr("y", 44)
+            .attr("x", sx2)
+            .attr("y", (sy * 3) + to)
             .text("Road begining");
+    //////////
 
     d3.select("#key")
             .append("line")
-            .attr("x1", 10)
-            .attr("y1", 60)
+            .attr("x1", 0)
+            .attr("y1", (sy * 4))
             .attr("x2", 40)
-            .attr("y2", 60)
+            .attr("y2", (sy * 4))
             .attr("stroke-width", 0.5)
             .attr("stroke", "darkgray")
             ;
@@ -121,10 +156,11 @@ d3.json(url, function (error, graph) {
     d3.select("#key")
             .append("text")
             .style("fill", "black")
-            .attr("x", 45)
-            .attr("y", 64)
+            .attr("x", sx2)
+            .attr("y", (sy * 4) + to)
             .text("Road end");
     ;
+
 });
 
 function update() {
@@ -136,14 +172,14 @@ function draw() {
     //drawNetwork();
     drawCars();
 }
-function state(){
-    for(var i =0; i < cars.length; i++){
+function state() {
+    for (var i = 0; i < cars.length; i++) {
         console.log("car: " + JSON.stringify(cars[i]));
     }
     console.log("----------")
-    for(var i =0; i < gisModel.network.length; i++){
+    for (var i = 0; i < gisModel.network.length; i++) {
         console.log("road: " + JSON.stringify(gisModel.network[i]));
-    } 
+    }
 }
 
 function drawCars() {
@@ -160,7 +196,6 @@ function updateCars() {
     for (var i = 0; i < cars.length; i++) {
         //console.log(".........")
         //console.log("UC car: " + JSON.stringify(cars[i]));
-
         var car = cars[i];
         //console.log("UC tail[0]: " + JSON.stringify(trafficmodel.firstTailSegment(car)) );
         var nextCoord = {x: car.coordinates[0].x, y: car.coordinates[0].y};
@@ -169,12 +204,11 @@ function updateCars() {
         var roadSegment = trafficmodel.firstTailSegment(car);
         // velocity
         //console.log("UC roadSegment: " + roadSegment);
-        nextCoord.x += trafficmodel.getDVX(roadSegment);
-        nextCoord.y += trafficmodel.getDVY(roadSegment);
+        nextCoord.x += trafficmodel.getDVX(car, roadSegment);
+        nextCoord.y += trafficmodel.getDVY(car, roadSegment);
 
         var nextSeg = null;
 
-        //switch (getUpdateStatus(network[car.tail[0].road], roadSegment, newCoord)) {
         switch (trafficmodel.getState(car, nextCoord)) {
             case 0: // full stop in place
                 //console.log("0 full stop ");
@@ -182,8 +216,6 @@ function updateCars() {
                 break;
             case 1: // update location on current segment
                 //console.log("1 update location on current segment");
-                //setColour(Color.GRAY); // steady as you go GRAY
-                //this.get(0).assign(nextCoord);
                 car.coordinates[0].x = nextCoord.x;
                 car.coordinates[0].y = nextCoord.y;
                 break;
@@ -201,31 +233,27 @@ function updateCars() {
                     road: car.tail[car.tail.length - 1].road,
                     seg: car.tail[car.tail.length - 1].seg + 1
                 };
-
                 //console.log("UC idxPair: " + JSON.stringify(idxPairNext));
                 nextSeg = gisModel.getSegment(idxPairNext); // idxPair = {road: , seg: }
                 //console.log("UC nextSeg: " + JSON.stringify(nextSeg));
-
                 trafficmodel.addTailSegment(car, idxPairNext, nextSeg);
                 // adjust location
                 // estimate next coord along new segment
                 var estCoord = {// just for readability
-                    x: nextSeg.coordinates[0].x + trafficmodel.getDVX(nextSeg),
-                    y: nextSeg.coordinates[0].y + trafficmodel.getDVY(nextSeg)
+                    x: nextSeg.coordinates[0].x + trafficmodel.getDVX(car, nextSeg),
+                    y: nextSeg.coordinates[0].y + trafficmodel.getDVY(car, nextSeg)
                 };
                 car.coordinates[0].x = estCoord.x;
                 car.coordinates[0].y = estCoord.y;
                 //console.log("UC car: " + JSON.stringify(car));
-                 //console.log("UC done");
+                //console.log("UC done");
                 break;
             case 3: // extra road, extra segment but still has more segments
                 console.log("3 extra road, extra segment but still has more segments");
                 break;
             case 4: // extra road segement overshoot 
                 //console.log("4 extra road segement overshoot");
-
                 //console.log("UC car: " + JSON.stringify(cars[i]));
-
                 var nextRoadList = trafficmodel
                         .getNextRoadDecision(car);
                 //console.log("UC nextRoadList: " + JSON.stringify(nextRoadList));
@@ -234,12 +262,10 @@ function updateCars() {
                     seg: 0
                 };
                 //console.log("UC idxPair: " + JSON.stringify(idxPairNext));
-
                 //console.log("nextRoadList: "+JSON.stringify(nextRoadList));
                 nextSeg = gisModel.getSegment(idxPairNext); //nextSeg = nextRoadList.feature[0];
                 //console.log("UC nextSeg: " + JSON.stringify(nextSeg));
-
-                if (nextSeg == null) {
+                if (nextSeg === null) {
                     console.log("deadend");
                     // deadend
                     break;
@@ -250,25 +276,18 @@ function updateCars() {
                 trafficmodel.addTailSegment(car, idxPairNext, nextSeg);
                 // estimate next coord
                 var estCoord = {// just for readability
-                    x: nextSeg.coordinates[0].x + trafficmodel.getDVX(nextSeg),
-                    y: nextSeg.coordinates[0].y + trafficmodel.getDVY(nextSeg)
+                    x: nextSeg.coordinates[0].x + trafficmodel.getDVX(car, nextSeg),
+                    y: nextSeg.coordinates[0].y + trafficmodel.getDVY(car, nextSeg)
                 };
-
                 car.coordinates[0].x = estCoord.x;
                 car.coordinates[0].y = estCoord.y;
                 //console.log("UC car: " + JSON.stringify(car));
                 //console.log("UC done");
-
                 break;
             default:
                 console.log("undefined situation");
-
         }
-
-        //console.log("B car: " + JSON.stringify(car));
-
     }
-
 }
 
 function getAttitude() {
@@ -282,8 +301,8 @@ function getAttitude() {
 
 function loadCars() {
     var sz = gisModel.network.length;
-    //sz = 2;
-    for (var i = 0; i < sz; i++) {
+    var fireEngines = 1;
+    for (var i = 0; i < sz - fireEngines; i++) {
         var feature = gisModel.network[i].feature; // road
         // for (var k = 0; k < feature.length; k++) {
 
@@ -291,13 +310,38 @@ function loadCars() {
             idx: i,
             attitude: getAttitude(),
             coordinates: [{"x": feature[0].coordinates[0].x, "y": feature[0].coordinates[0].y}],
-            tail: [{road: i, seg: 0}]
+            tail: [{road: i, seg: 0}],
+            type: 1
+        };
+        cars.push(car);
+
+        // initialize the road
+        //feature[0].occupied = true;
+        feature[0].state.occupied = car.idx;
+        feature[0].tailingspeed = feature[0].speed * car.attitude;
+        //console.log("car: " + JSON.stringify(car));
+
+    }
+    for (var i = 0; i < fireEngines; i++) {
+        var feature = gisModel.network[i].feature; // road
+        // for (var k = 0; k < feature.length; k++) {
+
+        var car = {
+            idx: cars.length,
+            attitude: 1, // max speed
+            coordinates: [{"x": feature[0].coordinates[0].x, "y": feature[0].coordinates[0].y}],
+            tail: [{road: i, seg: 0}],
+            type: 2
         };
         cars.push(car);
         // initialize the road
-        feature[0].occupied = true;
+        //feature[0].occupied = true;
+        feature[0].state.occupied = car.idx;
+        feature[0].state.supressTraffic = true;
         feature[0].tailingspeed = feature[0].speed * car.attitude;
+        feature[1].state.supressTraffic = true;
         //console.log("car: " + JSON.stringify(car));
-        
+
     }
 }
+
